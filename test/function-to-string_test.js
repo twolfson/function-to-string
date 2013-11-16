8cbc385eccebceaff616d099fde3b33121b15f00
Todd Wolfson <todd@twolfson.com>
2013-11-16 14:34:42 -0800

    Tests are in and failing

--- test/function-to-string_test.js
+++ test/function-to-string_test.js
@@ -1,11 +1,29 @@
-var function_to_string = require('../lib/function-to-string.js');
+var assert = require('assert');
+var glob = require('glob');
+// var testFiles = glob.sync(__dirname + '/test-files/**/*.js');
+var testFiles = glob.sync(__dirname + '/test-files/**/noop.js');
+var functionToString = require('../');
 
-describe('function-to-string', function () {
-  before(function () {
+describe('functionToString', function () {
+  testFiles.forEach(function (testFile) {
+    describe('interpretting ' + testFile, function () {
+      before(function () {
+        var testCase = require(testFile);
+        this.testCase = testCase;
+        this.actualInfo = functionToString(testCase);
+      });
 
-  });
+      it.skip('has the expected name', function () {
 
-  it('', function () {
+      });
 
+      it('has the expected parameters', function () {
+        assert.deepEqual(this.testCase.params, this.actualInfo.parameters);
+      });
+
+      it('has the expected body', function () {
+        assert.strictEqual(this.testCase.body, this.actualInfo.body);
+      });
+    });
   });
 });
