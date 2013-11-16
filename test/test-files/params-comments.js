98a770456d520e1857b4260982490a9c2c95a50d
Todd Wolfson <todd@twolfson.com>
2013-11-16 14:28:02 -0800

    Adding test cases

---
+++ test/test-files/params-comments.js
@@ -0,0 +1,9 @@
+exports.fn = function paramsComments (/* Number */ data) {
+  return data;
+};
+
+exports.name = 'paramsComments';
+exports.params = ['data'];
+exports.body = [
+"  return data;"
+].join('\n');
